9dbf2336e19ccb9a1297e52d72294b87f2734a79
vinyborges <viny.bdj@gmail.com>
2023-05-09 23:25:47 -0700

    commit tree dd7e6605266daf7d2c6b4d60da2ca62c52db93b2

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const cats = [];
+cats.push('Milo','Otis','Garfield');
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function  destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    let copyCats = [...cats,name]
+    return copyCats;
+}
+
+function prependCat(name){
+    let copyOfCats = [name,...cats];
+    return copyOfCats;
+}
+
+function removeLastCat(){
+    let newCats = [...cats];
+    newCats.pop();
+    return newCats;
+}
+
+function removeFirstCat(){
+    let NewCopyCats = [...cats];
+    NewCopyCats.shift();
+    return NewCopyCats;
+}
+
+
+
+
+
